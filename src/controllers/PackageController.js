414b7cb4f2872576fd5a5e4ed785a6ac9a790524
João vitor Julietti Marques <joao.marquesjulietti@gmail.com>
2020-12-28 18:44:02 -0300

    Create function delete, update and get

--- src/controllers/PackageController.js
+++ src/controllers/PackageController.js
@@ -6,3 +6,15 @@ exports.saveRaw = async (data, trackerModel, cmd) => {
     const id = uuidv4();
     Package.create({ id, date, data, trackerModel, cmd });
 };
+
+exports.index = async (id) => {
+    Package.get(id)
+}
+
+exports.delete = async(id) => {
+    Package.delete(id)
+}
+
+exports.update = async(id, date, data, trackerModel, cmd) => {
+    Package.update({"id": id}, {"date": date}, {"data": data}, {"trackerModel": trackerModel}, {"cmd": cmd})
+}
